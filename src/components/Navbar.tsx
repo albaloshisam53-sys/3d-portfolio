67330f4c67c36d5d1af6e5e987ff7a7a361f316b
albaloshisam53-sys <albaloshisam53@gmail.com>
2026-05-01 13:17:21 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,16 +43,16 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
+          SB
         </a>
         <a
-          href="https://www.linkedin.com/in/akashrmalhotra/"
+          href="https://www.linkedin.com/in/muhammad-sarim-baloch-4b3362246/"
           className="navbar-connect"
           data-cursor="disable"
           target="_blank"
           rel="noreferrer"
         >
-          linkedin.com/in/akashrmalhotra
+          linkedin.com/in/muhammad-sarim-baloch
         </a>
         <ul>
           <li>
